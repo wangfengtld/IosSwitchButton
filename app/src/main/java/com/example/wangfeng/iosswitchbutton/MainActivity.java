2a4e489139870e542334ab0bd2ece249777c44c8
wangfeng <wangfengtld@126.com>
2015-05-09 10:11:26 +0800

    ios的switch实现

---
+++ app/src/main/java/com/example/wangfeng/iosswitchbutton/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.wangfeng.iosswitchbutton;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    SwitchButton switchButton = (SwitchButton) findViewById(R.id.switch_button);
+    switchButton.setChecked(true);
+    switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b) {
+          Toast.makeText(MainActivity.this, "选中", Toast.LENGTH_SHORT).show();
+        } else {
+          Toast.makeText(MainActivity.this, "没选中", Toast.LENGTH_SHORT).show();
+        }
+      }
+    });
+  }
+
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+}
